65fadf41e1f94241a5e502b70114fe653e1136b7
Zach Gastley <zach@Zachs-Mac-mini.attlocal.net>
2024-12-17 17:19:44 -0500

    updated highlight-whitespace config

--- lua/plugins/highlight-whitespace.lua
+++ lua/plugins/highlight-whitespace.lua
@@ -1,4 +1,35 @@
 return {
   "lukoshkin/highlight-whitespace",
-  config = true,
+  opts = {
+    tws = "\\s\\+$",
+    clear_on_bufleave = false,
+    palette = {
+      -- :echo getcompletion('', 'filetype')
+      -- :echo &filetype
+      markdown = {
+        tws = "RosyBrown",
+        ["\\(\\S\\)\\@<=\\s\\(\\.\\|,\\)\\@="] = "CadetBlue3",
+        ["\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@="] = "SkyBlue1",
+        ["\\t\\+"] = "plum4",
+      },
+      terraform = {
+        tws = "PaleVioletRed",
+        ["\\(\\S\\)\\@<=\\s\\(,\\)\\@="] = "coral1",
+        ["\\(#\\|--\\)\\@<= \\{2,\\}\\(\\S\\)\\@="] = "#3B3B3B",
+        ["\\t\\+"] = "plum4",
+      },
+      other = {
+        tws = "PaleVioletRed",
+        ["\\t\\+$"] = "plum4",
+        -- ['\\(\\S\\)\\@<=\\s\\(,\\)\\@='] = 'coral1',
+        -- ["\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@="] = "LightGoldenrod3",
+        -- ['\\(#\\|--\\)\\@<= \\{2,\\}\\(\\S\\)\\@='] = '#3B3B3B',
+        -- ['\\t\\+'] = 'plum4',
+        -- ['\\S\\@<=\\s,\\@='] = 'coral1',
+        -- ['\\S\\@<=\\(#\\|--\\)\\@<! \\{2,3\\}\\S\\@=\\(#\\|--\\)\\@!'] = 'LightGoldenrod3',
+        -- ['\\(#\\|--\\)\\@<= \\{2,\\}\\S\\@='] = '#3B3B3B',
+        -- ['\\S\\@<= \\{3,\\}\\(#\\|--\\)\\@='] = '#3B3B3B',
+      },
+    },
+  },
 }
